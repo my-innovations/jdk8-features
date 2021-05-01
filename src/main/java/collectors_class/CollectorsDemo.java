c290f484dcef4a5324aac4f41393dfe2ab5d39d8
DELL <punyasmrutin@gmail.com>
2021-05-01 12:38:52 +0530

    modified files

--- src/main/java/collectors_class/CollectorsDemo.java
+++ src/main/java/collectors_class/CollectorsDemo.java
@@ -2,9 +2,11 @@ package collectors_class;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.IntSummaryStatistics;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -15,44 +17,56 @@ import model.Student;
 
 public class CollectorsDemo {
 
+	private static double[] doubleArrayprimitive = { 2.5d, 7.8d, 4.9d };
 	private static Double[] doubleArray = { 2.5d, 7.8d, 4.9d };
 	private static List<String> namesList = Arrays.asList("Jon", "Ajeet", "Steve", "Ajeet", "Jon", "Ajeet");
 
 	private static List<Product> productsList = Arrays.asList(
-			new Product(1, "HP Laptop", 25000f),
-			new Product(2, "Dell Laptop", 30000f), 
-			new Product(5, "Apple Laptop", 90000f),
-			new Product(3, "Lenevo Laptop", 28000f), 
-			new Product(4, "Sony Laptop", 28000f));
+		new Product(1, "HP Laptop", 25000f),
+		new Product(2, "Dell Laptop", 30000f), 
+		new Product(5, "Apple Laptop", 90000f),
+		new Product(3, "Lenevo Laptop", 28000f), 
+		new Product(4, "Sony Laptop", 28000f));
 	
-	private static List<Student> studenstlist = Arrays.asList(
-			new Student(11, "Jon", 22), 
-			new Student(22, "Steve", 18),
-			new Student(33, "Lucy", 22), 
-			new Student(55, "Maggie", 18));
+	private static List<Student> studentslist = Arrays.asList(
+		new Student(11, "Jon", 22), 
+		new Student(22, "Steve", 19),
+		new Student(33, "Lucy", 23), 
+		new Student(55, "Maggie", 18));
 
 	public static void main(String[] args) {
 		
 		sumOfAllItemsOfDoubleArray();
-		findNoOfOccurancesOfItemsInArray();
+		findNoOfOccurancesOfEachItemInAnArray();
 		gatherAllProductPricesInList();
 		sumOfAllprices();
 		gatherProductIdWithName();
 		gatherProductNameWithPrice();
 		countTotalNoOfProducts();
-		allStudNames();
+		getAllStudentNames();
+		findStudentWithMaxAge();
+		findStudentWithMinAge();
+		findStudentWithAgeGreaterThan11();
+		summeryStatistics();
 
 	}
 
 	public static void sumOfAllItemsOfDoubleArray() {
 		Double d = Stream.of(doubleArray).reduce(0.0, (Double d1, Double d2) -> d1 + d2);
+		// Double d = Collectors.computeFinalSum(doubleArrayprimitive);
 		System.out.println(d);
 
+		//product of all items of the array
+		Double res = (Double) Stream.of(doubleArray).collect(Collectors.reducing(1d, (num1, num2) -> num1 * num2));
+		System.out.println(res);
 	}
 
-	public static void findNoOfOccurancesOfItemsInArray() {
+	public static void findNoOfOccurancesOfEachItemInAnArray() {
 		Map<String, Long> map = namesList.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 		System.out.println(map);
+		
+		Map<String, Long> counts = namesList.stream().collect(Collectors.groupingBy(item -> item, Collectors.counting()));
+		System.out.println(counts);
 	}
 
 	public static void gatherAllProductPricesInList() {
@@ -70,15 +84,18 @@ public class CollectorsDemo {
 
 	public static void sumOfAllprices() {
 
-		// sum of all ids
+		// sum of all ids of all products
 		int sumId = productsList.stream().collect(Collectors.summingInt(product -> product.id));
 		System.out.println("Sum of all id's: " + sumId);
+		
+		Double average2 = productsList.stream().collect(Collectors.averagingInt(product -> product.id));
+		System.out.println("Average id is: " + average2);
 
-		// summ of all the prices
+		// summ of all the prices of all products
 		Double sumPrices = productsList.stream().collect(Collectors.summingDouble(product -> product.price));
 		System.out.println("Sum of all prices: " + sumPrices); // Sum of prices: 201000.0
 
-		// get average Price
+		// get average Price of all products
 		Double average = productsList.stream().collect(Collectors.averagingDouble(product -> product.price));
 		System.out.println("Average price is: " + average);
 
@@ -117,29 +134,60 @@ public class CollectorsDemo {
 
 	public static void countTotalNoOfProducts() {
 
-		// Counting Elements
 		Long noOfElements = productsList.stream().collect(Collectors.counting());
 		System.out.println("Total no of products : " + noOfElements);
-
 	}
 
-	public static void allStudNames() {
+	public static void getAllStudentNames() {
 
-		// gathering all student where id > 22
-		List<Student> students = studenstlist.stream().filter(stud -> stud.id > 22).collect(Collectors.toList());
-		students.forEach(System.out::println);
-
-		// Fetching student data as a Set
-		Set<Student> students2 = studenstlist.stream().filter(stud -> stud.id > 22).collect(Collectors.toSet());
-		students2.forEach(System.out::println);
-
-		// gathering all stud names
-		List<String> namesList = studenstlist.stream().map(stud -> stud.name).collect(Collectors.toList());
+		List<String> namesList = studentslist.stream().map(stud -> stud.name).collect(Collectors.toList());
 		namesList.forEach(System.out::println);
-
-		// Getting the average age of the students.
-		Double avgAge = studenstlist.stream().collect(Collectors.averagingInt(student -> student.age));
+	}
+	
+	public static void getAverageAgeOfAllStudents() {
+		
+		Double avgAge = studentslist.stream().collect(Collectors.averagingDouble(student -> student.age));
 		System.out.println("Average Age of Students is: " + avgAge);
+	}
+	
+	public static void findStudentWithMaxAge() {
+		Comparator<Student> c = (Student user1, Student user2) -> user1.getAge() - user2.getAge();
+		Optional<Student> s = studentslist.stream().collect(Collectors.maxBy(c));
+		//Optional<Student> s = studenstlist.stream().collect(Collectors.maxBy(Comparator..reverseOrder()));
+		System.out.println(s.get());
+	}
+	
+	public static void findStudentWithAgeGreaterThan11() {
+		Map<Boolean, List<Student> > map = studentslist.stream().collect(Collectors.partitioningBy(s -> s.age > 20));
+		System.out.println(map);
+		
+		
+	}
+	
+	public static void summeryStatistics() {
+		//Using Collectors.summarizingInt()
+	    IntSummaryStatistics intSummaryStatistics = studentslist
+	        .stream()
+	        .collect(Collectors.summarizingInt(Student::getAge));
+	    System.out.println("IntSummaryStatistics for age: " + intSummaryStatistics);
 
+	}
+	
+	
+	
+	public static void findStudentWithMinAge() {
+		Comparator<Student> c = (Student user1, Student user2) -> user1.getAge() - user2.getAge();
+		Optional<Student> s = studentslist.stream().collect(Collectors.minBy(c));
+		System.out.println(s.get());
+	}
+	
+	
+	public static void getAllStudentsWhereIdGreaterThan22() {
+		
+		List<Student> studentsList = studentslist.stream().filter(stud -> stud.id > 22).collect(Collectors.toList());
+		studentsList.forEach(System.out::println);
+		//OR
+		Set<Student> studentsSet = studentslist.stream().filter(stud -> stud.id > 22).collect(Collectors.toSet());
+		studentsSet.forEach(System.out::println);
 	}
 }
--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -1,6 +1,7 @@
 package model;
 
 public class Student {
+	
 	public int id;
 	public String name;
 	public int age;
@@ -10,4 +11,34 @@ public class Student {
 		this.name = name;
 		this.age = age;
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", age=" + age + "]";
+	}
+
 }
